e7c3598b13d939b221b6efd09bee684771e0f194
Cloudy <cloud.kira@gmail.com>
2020-05-06 21:58:43 +0200

    added button icons and listeners

--- Soundroid/app/src/main/java/com/example/soundroid/ui/player/MusicPlayerFragment.java
+++ Soundroid/app/src/main/java/com/example/soundroid/ui/player/MusicPlayerFragment.java
@@ -7,8 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -32,6 +34,15 @@ public class MusicPlayerFragment extends Fragment {
     private TextView title;
     private TextView album;
     private TextView artist;
+
+    private Button playButton;
+    private boolean isPlaying = false;
+
+    private Button toggleMute;
+    private boolean isMute = false;
+
+    private Button previousSong;
+    private Button nextSong;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -59,6 +70,8 @@ public class MusicPlayerFragment extends Fragment {
         album = root.findViewById(R.id.album_name_player);
         album_art = root.findViewById(R.id.album_art_player);
 
+        setButtonListeners(root);
+
         return root;
     }
 
@@ -71,8 +84,85 @@ public class MusicPlayerFragment extends Fragment {
         album.setText("unknown album");
     }
 
+    private void setButtonListeners(View root) {
+        playButton = root.findViewById(R.id.toggle_play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPlayButtonClick(v);
+            }
+        });
 
+        toggleMute = root.findViewById(R.id.toggle_sound_button);
+        toggleMute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onToggleMuteClick(v);
+            }
+        });
+
+        previousSong = root.findViewById(R.id.previous_song_button);
+        previousSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPreviousSongClick(v);
+            }
+        });
+
+        nextSong = root.findViewById(R.id.next_song_button);
+        nextSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNextSongClick(v);
+            }
+        });
+    }
+
+    private void onPlayButtonClick(View v) {
+        if(!isPlaying) {
+            Toast toast = Toast.makeText(v.getContext(), "Play music", Toast.LENGTH_SHORT);
+            toast.show();
+            playButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_media_pause, 0, 0, 0);
+            isPlaying = true;
+        } else {
+            Toast toast = Toast.makeText(v.getContext(), "Pause music", Toast.LENGTH_SHORT);
+            toast.show();
+            playButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_media_play, 0, 0, 0);
+            isPlaying = false;
+        }
+    }
+
+    private void onToggleMuteClick(View v) {
+        if(!isMute) {
+            Toast toast = Toast.makeText(v.getContext(), "Mute", Toast.LENGTH_SHORT);
+            toast.show();
+            toggleMute.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_media_mute, 0, 0, 0);
+            isMute = true;
+        } else {
+            Toast toast = Toast.makeText(v.getContext(), "Unmute", Toast.LENGTH_SHORT);
+            toast.show();
+            toggleMute.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_media_unmute, 0, 0, 0);
+            isMute = false;
+        }
+    }
+
+    private void onPreviousSongClick(View v) {
+        Toast toast = Toast.makeText(v.getContext(), "Play previous song", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    private void onNextSongClick(View v) {
+        Toast toast = Toast.makeText(v.getContext(), "Play next song", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
     public void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
